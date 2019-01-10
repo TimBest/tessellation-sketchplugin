ccfda23ef5c0b7783033acd95bc35760fc24c849
Tim <hello@timbest.net>
2019-01-10 14:58:23 -0800

    remove artboard non sense

--- my-plugin.sketchplugin/Contents/Sketch/my-command.js
+++ my-plugin.sketchplugin/Contents/Sketch/my-command.js
@@ -107,44 +107,42 @@ var Document = __webpack_require__(/*! sketch/dom */ "sketch/dom").Document;
 var Shape = __webpack_require__(/*! sketch/dom */ "sketch/dom").Shape;
 
 var Rectangle = __webpack_require__(/*! sketch/dom */ "sketch/dom").Rectangle; // also exposed on Document
+// function getSelectedArtboards() {
+//   var document = Document.getSelectedDocument();
+//   var page = document.selectedPage;
+//   var selectedLayers = document.selectedLayers;
+//   var selectedCount = selectedLayers.length;
+//
+//   if (selectedCount === 0) {
+//     return [];
+//   }
+//
+//   var artboards = [];
+//   selectedLayers.forEach(function(layer) {
+//     if (layer.type === "Artboard") {
+//       artboards.push(layer);
+//     }
+//   });
+//   return artboards;
+// }
+// var artboards = getSelectedArtboards();
+//
+// if (artboards.length === 0) {
+//   UI.message('No Artboard selected');
+//   return;
+// }
+//
+// if (artboards.length > 1) {
+//   UI.message('Too many Artboards selected. Select a single Artboard');
+//   return;
+// }
+//
+// var artboard = artboards[0];
 
-
-function getSelectedArtboards() {
-  var document = Document.getSelectedDocument();
-  var page = document.selectedPage;
-  var selectedLayers = document.selectedLayers;
-  var selectedCount = selectedLayers.length;
-
-  if (selectedCount === 0) {
-    return [];
-  }
-
-  var artboards = [];
-  selectedLayers.forEach(function (layer) {
-    if (layer.type === "Artboard") {
-      artboards.push(layer);
-    }
-  });
-  return artboards;
-}
 
 /* harmony default export */ __webpack_exports__["default"] = (function () {
-  var artboards = getSelectedArtboards();
-
-  if (artboards.length === 0) {
-    UI.message('No Artboard selected');
-    return;
-  }
-
-  if (artboards.length > 1) {
-    UI.message('Too many Artboards selected. Select a single Artboard');
-    return;
-  }
-
-  var artboard = artboards[0];
-  var artboardLayers = artboard.layers.map(function (item) {
-    return item.sketchObject;
-  });
+  var document = Document.getSelectedDocument();
+  var page = document.selectedPage;
   var frame = new Rectangle(0, 0, 100, 100);
   var shape = new Shape({
     name: 'my shape',
@@ -153,7 +151,10 @@ function getSelectedArtboards() {
       fills: ['#4A90E2']
     }
   });
-  artboard.layers = artboardLayers.concat(shape);
+  var pageLayers = page.layers.map(function (item) {
+    return item.sketchObject;
+  });
+  page.layers = pageLayers.concat(shape);
 });
 
 /***/ }),
--- src/my-command.js
+++ src/my-command.js
@@ -5,44 +5,41 @@ var Rectangle = require('sketch/dom').Rectangle
 
 // also exposed on Document
 
-function getSelectedArtboards() {
-  var document = Document.getSelectedDocument();
-  var page = document.selectedPage;
-  var selectedLayers = document.selectedLayers;
-  var selectedCount = selectedLayers.length;
-
-  if (selectedCount === 0) {
-    return [];
-  }
-
-  var artboards = [];
-  selectedLayers.forEach(function(layer) {
-    if (layer.type === "Artboard") {
-      artboards.push(layer);
-    }
-  });
-  return artboards;
-}
-
+// function getSelectedArtboards() {
+//   var document = Document.getSelectedDocument();
+//   var page = document.selectedPage;
+//   var selectedLayers = document.selectedLayers;
+//   var selectedCount = selectedLayers.length;
+//
+//   if (selectedCount === 0) {
+//     return [];
+//   }
+//
+//   var artboards = [];
+//   selectedLayers.forEach(function(layer) {
+//     if (layer.type === "Artboard") {
+//       artboards.push(layer);
+//     }
+//   });
+//   return artboards;
+// }
+// var artboards = getSelectedArtboards();
+//
+// if (artboards.length === 0) {
+//   UI.message('No Artboard selected');
+//   return;
+// }
+//
+// if (artboards.length > 1) {
+//   UI.message('Too many Artboards selected. Select a single Artboard');
+//   return;
+// }
+//
+// var artboard = artboards[0];
 
 export default function() {
-  var artboards = getSelectedArtboards();
-
-  if (artboards.length === 0) {
-    UI.message('No Artboard selected');
-    return;
-  }
-
-  if (artboards.length > 1) {
-    UI.message('Too many Artboards selected. Select a single Artboard');
-    return;
-  }
-
-  var artboard = artboards[0];
-
-  var artboardLayers = artboard.layers.map(function(item) {
-    return item.sketchObject;
-  });
+  var document = Document.getSelectedDocument();
+  var page = document.selectedPage;
 
   var frame = new Rectangle(0, 0, 100, 100);
   var shape = new Shape({
@@ -53,7 +50,11 @@ export default function() {
     }
   });
 
-  artboard.layers = artboardLayers.concat(shape);
+  var pageLayers = page.layers.map(function(item) {
+    return item.sketchObject;
+  });
+
+  page.layers = pageLayers.concat(shape);
 
 
 }
